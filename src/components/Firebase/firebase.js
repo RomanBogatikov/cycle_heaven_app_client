b9af12ca9d90361571ca51d82b9e9a738153b9fd
RomanBogatikov <ivaroman2000@gmail.com>
2019-06-05 22:29:42 -0400

    test

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,6 +2,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appID: process.env.REACT_APP_APP_ID,
+}
+
 // const prodConfig = {
 //   apiKey: process.env.REACT_APP_API_KEY,
 //   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -11,7 +21,6 @@ import 'firebase/auth';
 //   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 //   appID: process.env.REACT_APP_APP_ID,
 // }
-
 // const devConfig = {
 //   apiKey: process.env.REACT_APP_API_KEY,
 //   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -22,43 +31,10 @@ import 'firebase/auth';
 //   appID: process.env.REACT_APP_APP_ID,
 // }
 
+// if (process.env.NODE_ENV === 'production') {console.log('process.env.NODE_ENV=production') };
+// if (process.env.NODE_ENV === 'development') {console.log('process.env.NODE_ENV=development') };
+
 // const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
-
-// const config = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appID: process.env.REACT_APP_APP_ID,
-// }
-
-const prodConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appID: process.env.REACT_APP_APP_ID,
-}
-const devConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appID: process.env.REACT_APP_APP_ID,
-}
-
-
-
-if (process.env.NODE_ENV === 'production') {console.log('process.env.NODE_ENV=production') };
-if (process.env.NODE_ENV === 'development') {console.log('process.env.NODE_ENV=development') };
-
-const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 class Firebase {
   constructor() {
