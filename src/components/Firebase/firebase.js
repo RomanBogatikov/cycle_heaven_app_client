ba9c1384927c8bb59701bd3f250b978209f12cb1
RomanBogatikov <ivaroman2000@gmail.com>
2019-06-05 20:12:49 -0400

    test to deploy

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,7 +2,29 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 
-const prodConfig = {
+// const prodConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appID: process.env.REACT_APP_APP_ID,
+// }
+
+// const devConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appID: process.env.REACT_APP_APP_ID,
+// }
+
+// const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -11,18 +33,6 @@ const prodConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appID: process.env.REACT_APP_APP_ID,
 }
-
-const devConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appID: process.env.REACT_APP_APP_ID,
-}
-
-const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 console.log('config=', config);
 
